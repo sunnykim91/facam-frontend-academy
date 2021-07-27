9095deadaec1e37d0da4ef49b8a994b23d02d2ce
unknown <tjsgnl2002@gmail.com>
2021-07-27 21:21:46 +0900

    CH03_06 페이징 구현하기 완료

--- HackerNewsClient/app.js
+++ HackerNewsClient/app.js
@@ -4,6 +4,10 @@ const content = document.querySelector("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
+const store = {
+    currentPage: 1,
+};
+
 function getData(url) {
     ajax.open("GET", url, false);
     ajax.send();
@@ -19,10 +23,10 @@ function newsFeed() {
 
     newsList.push("<ul>");
 
-    for (let i = 0; i <= 10; i++) {
+    for (let i = store.currentPage - 1; i <= store.currentPage * 10; i++) {
         newsList.push(`
             <li>
-                <a href="#${newsFeed[i].id}">
+                <a href="#/show/${newsFeed[i].id}">
                 ${newsFeed[i].title}(${newsFeed[i].comments_count})
                 </a>
             </li>
@@ -31,11 +35,20 @@ function newsFeed() {
 
     newsList.push("</ul>");
 
+    newsList.push(`
+        <div>
+            <a href="#/page/${
+                store.currentPage > 1 ? store.currentPage - 1 : 1
+            }">이전 페이지</a>
+            <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+        </div>
+    `);
+
     container.innerHTML = newsList.join("");
 }
 
 function newsDetail() {
-    const id = location.hash.substr(1);
+    const id = location.hash.substr(7);
 
     const newsContent = getData(CONTENT_URL.replace("@id", id));
     const title = document.createElement("h1");
@@ -44,7 +57,7 @@ function newsDetail() {
             <h1>${newsContent.title}</h1>
     
             <div>
-                <a href="#">목록으로</a>
+                <a href="#/page/${store.currentPage}">목록으로</a>
             </div>
         `;
 }
@@ -53,6 +66,9 @@ function router() {
     const routePath = location.hash;
 
     if (routePath === "") {
+        newsFeed();
+    } else if (routePath.indexOf("#/page/") >= 0) {
+        store.currentPage = Number(routePath.substr(7));
         newsFeed();
     } else {
         newsDetail();
